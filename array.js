0f1758811cd07a3b3053aa886fd3b67d55f3e9c7
shuvo3837 <shuvosd3837@gmail.com>
2026-05-08 01:40:20 +0600

    added array method

--- array.js
+++ array.js
@@ -4,6 +4,24 @@ const products = [
     {name: 'pc', price: 30000}
 ]
 
+products.forEach(product => console.log(product));
+
 // map method
 const product_names = products.map(product => product.name );
 console.log(product_names);
+
+// filter method
+const expensive_products = products.filter(product => product.price>20000);
+console.log(expensive_products);
+
+// reduce method
+const total_price = products.reduce((total, product)=> total + product.price, 0);
+console.log(total_price);
+
+// find method
+const phone = products.find(product => product.name === 'phone');
+console.log(phone);
+
+// some method
+const has_expensive_product = products.some(product => product.price > 20000);
+console.log(has_expensive_product);
